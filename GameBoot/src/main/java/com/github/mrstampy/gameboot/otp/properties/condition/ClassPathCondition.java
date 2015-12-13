514f0dec68de5e63b0974e177b45ec30ae21611d
burton <burt.alexander@gmail.com>
2015-12-14 06:28:48 +1000

    separated out OTP properties for ease of exclusion and overriding

---
+++ GameBoot/src/main/java/com/github/mrstampy/gameboot/otp/properties/condition/ClassPathCondition.java
@@ -0,0 +1,60 @@
+/*
+ *              ______                        ____              __ 
+ *             / ____/___ _____ ___  ___     / __ )____  ____  / /_
+ *            / / __/ __ `/ __ `__ \/ _ \   / __  / __ \/ __ \/ __/
+ *           / /_/ / /_/ / / / / / /  __/  / /_/ / /_/ / /_/ / /_  
+ *           \____/\__,_/_/ /_/ /_/\___/  /_____/\____/\____/\__/  
+ *                                                 
+ *                                 .-'\
+ *                              .-'  `/\
+ *                           .-'      `/\
+ *                           \         `/\
+ *                            \         `/\
+ *                             \    _-   `/\       _.--.
+ *                              \    _-   `/`-..--\     )
+ *                               \    _-   `,','  /    ,')
+ *                                `-_   -   ` -- ~   ,','
+ *                                 `-              ,','
+ *                                  \,--.    ____==-~
+ *                                   \   \_-~\
+ *                                    `_-~_.-'
+ *                                     \-~
+ *
+ *
+ * Copyright (C) 2015 Burton Alexander
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * 
+ */
+package com.github.mrstampy.gameboot.otp.properties.condition;
+
+import com.github.mrstampy.gameboot.util.resource.AbstractFallbackResourceCondition;
+
+/**
+ * The Class ClassPathCondition.
+ */
+public class ClassPathCondition extends AbstractFallbackResourceCondition {
+
+  /** The Constant OTP_PROPERTIES. */
+  public static final String OTP_PROPERTIES = "classpath:otp.properties";
+
+  /**
+   * Instantiates a new class path condition.
+   */
+  public ClassPathCondition() {
+    super(OTP_PROPERTIES, FileCondition.OTP_PROPERTIES, ExternalClassPathCondition.OTP_PROPERTIES);
+  }
+
+}
