6dc2e69229e78969e3429b68bf3e4f211c427f83
burton <burt.alexander@gmail.com>
2015-12-14 17:39:24 +1000

    Including groovy, Logback now configured w/ groovy

---
+++ GameBoot/src/main/resources/logback.groovy
@@ -0,0 +1,77 @@
+/*
+ *              ______                        ____              __ 
+ *             / ____/___ _____ ___  ___     / __ )____  ____  / /_
+ *            / / __/ __ `/ __ `__ \/ _ \   / __  / __ \/ __ \/ __/
+ *           / /_/ / /_/ / / / / / /  __/  / /_/ / /_/ / /_/ / /_  
+ *           \____/\__,_/_/ /_/ /_/\___/  /_____/\____/\____/\__/  
+ *                                                 
+ *                                 .-'\
+ *                              .-'  `/\
+ *                           .-'      `/\
+ *                           \         `/\
+ *                            \         `/\
+ *                             \    _-   `/\       _.--.
+ *                              \    _-   `/`-..--\     )
+ *                               \    _-   `,','  /    ,')
+ *                                `-_   -   ` -- ~   ,','
+ *                                 `-              ,','
+ *                                  \,--.    ____==-~
+ *                                   \   \_-~\
+ *                                    `_-~_.-'
+ *                                     \-~
+ * 
+ *                       http://mrstampy.github.io/gameboot/
+ *
+ * Copyright (C) 2015 Burton Alexander
+ * 
+ * Default Logback configuration for GameBoot
+ *
+ * http://logback.qos.ch/
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * 
+ */
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.rolling.FixedWindowRollingPolicy
+import ch.qos.logback.core.rolling.RollingFileAppender
+import ch.qos.logback.core.rolling.SizeBasedTriggeringPolicy
+
+import static ch.qos.logback.classic.Level.DEBUG
+import static ch.qos.logback.classic.Level.WARN
+
+scan()
+appender("CONSOLE", ConsoleAppender) {
+  encoder(PatternLayoutEncoder) {
+    pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
+  }
+}
+appender("FILE", RollingFileAppender) {
+  file = "gameboot.log"
+  rollingPolicy(FixedWindowRollingPolicy) {
+    fileNamePattern = "gameboot.%i.log.zip"
+    minIndex = 1
+    maxIndex = 10
+  }
+  triggeringPolicy(SizeBasedTriggeringPolicy) {
+    maxFileSize = "50MB"
+  }
+  encoder(PatternLayoutEncoder) {
+    pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
+  }
+}
+logger("com.github.mrstampy.gameboot", DEBUG)
+logger("com.github.mrstampy.gameboot.util.resource.ResourceLogger", DEBUG)
+root(WARN, ["CONSOLE", "FILE"])
