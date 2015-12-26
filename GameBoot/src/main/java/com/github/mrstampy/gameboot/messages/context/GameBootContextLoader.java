25e35ecf298d7c979fa988593b55c2b7dabbb67e
burton <burt.alexander@gmail.com>
2015-12-26 12:08:46 +1000

    int'l.

--- GameBoot/src/main/java/com/github/mrstampy/gameboot/messages/context/GameBootContextLoader.java
+++ GameBoot/src/main/java/com/github/mrstampy/gameboot/messages/context/GameBootContextLoader.java
@@ -49,12 +49,14 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
+import java.util.ResourceBundle;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.io.Resource;
@@ -62,7 +64,12 @@ import org.springframework.core.io.Resource;
 import com.github.mrstampy.gameboot.util.resource.AbstractFallbackResourceCondition;
 
 /**
- * The Class GameBootErrorLoader.
+ * The Class GameBootErrorLoader. {@link Locale} driven, the file
+ * 'errors.properties' must exist as the root file. {@link Locale} specific
+ * files are named as per property {@link ResourceBundle}s ie.
+ * 'errors_en_CA.properties' or 'errors_fr.properties'. Lookups are performed
+ * from specific -> general, '_[lang code]_[country code]' first, then '_[lang
+ * code]', then the root 'errors.properties'.
  */
 public class GameBootContextLoader implements ApplicationContextAware, ResponseContextLoader {
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
@@ -72,6 +79,9 @@ public class GameBootContextLoader implements ApplicationContextAware, ResponseC
   private static final String DESCRIPTION = ".description";
 
   private ApplicationContext ctx;
+
+  @Value("#{'${game.boot.additional.locales}'.split(',')}")
+  private List<String> additionalLocales;
 
   /*
    * (non-Javadoc)
@@ -95,6 +105,14 @@ public class GameBootContextLoader implements ApplicationContextAware, ResponseC
       String suffix = "_" + locale.getLanguage();
       addToMap(map, suffix);
     }
+
+    additionalLocales.forEach(suffix -> {
+      try {
+        addToMap(map, suffix);
+      } catch (Exception e) {
+        log.error("Unexpected exception", e);
+      }
+    });
 
     Map<Integer, ResponseContext> root = getForLocale(GameBootContextLookup.ROOT);
     if (root == null) throw new IllegalStateException("No error.properties file");
@@ -134,7 +152,7 @@ public class GameBootContextLoader implements ApplicationContextAware, ResponseC
     Resource r = getOverridableErrorResource(suffix);
 
     if (r == null || !r.exists()) {
-      log.trace("No error.properties for {}", suffix);
+      log.debug("No error.properties for {}", suffix);
       return null;
     }
 
