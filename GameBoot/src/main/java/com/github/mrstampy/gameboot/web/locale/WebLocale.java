1bf0fc94ba2975a42fbda9a299a6c4b53cedc172
burton <burt.alexander@gmail.com>
2016-01-02 13:53:30 +1000

    utility class for web locale setting

---
+++ GameBoot/src/main/java/com/github/mrstampy/gameboot/web/locale/WebLocale.java
@@ -0,0 +1,75 @@
+/*
+ *              ______                        ____              __ 
+ *             / ____/___ _____ ___  ___     / __ )____  ____  / /_
+ *            / / __/ __ `/ __ `__ \/ _ \   / __  / __ \/ __ \/ __/
+ *           / /_/ / /_/ / / / / / /  __/  / /_/ / /_/ / /_/ / /_  
+ *           \____/\__,_/_/ /_/ /_/\___/  /_____/\____/\____/\__/  
+ *                                                 
+ *                                 .-'\
+ *                              .-'  `/\
+ *                           .-'      `/\
+ *                           \         `/\
+ *                            \         `/\
+ *                             \    _-   `/\       _.--.
+ *                              \    _-   `/`-..--\     )
+ *                               \    _-   `,','  /    ,')
+ *                                `-_   -   ` -- ~   ,','
+ *                                 `-              ,','
+ *                                  \,--.    ____==-~
+ *                                   \   \_-~\
+ *                                    `_-~_.-'
+ *                                     \-~
+ * 
+ *                       http://mrstampy.github.io/gameboot/
+ *
+ * Copyright (C) 2015, 2016 Burton Alexander
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * 
+ */
+package com.github.mrstampy.gameboot.web.locale;
+
+import java.util.Locale;
+
+import javax.servlet.ServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.github.mrstampy.gameboot.locale.processor.LocaleRegistry;
+import com.github.mrstampy.gameboot.systemid.SystemIdKey;
+
+/**
+ * Utility class to set the {@link Locale} based upon the client's
+ * {@link ServletRequest}.
+ */
+public class WebLocale {
+
+  @Autowired
+  private LocaleRegistry registry;
+
+  /**
+   * Sets the locale.
+   *
+   * @param request
+   *          the request
+   * @param key
+   *          the key
+   */
+  public void setLocale(ServletRequest request, SystemIdKey key) {
+    Locale locale = request.getLocale();
+
+    if (locale != null) registry.put(key, locale);
+  }
+}
